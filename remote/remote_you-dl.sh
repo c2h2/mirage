2a0787d7174c12225a034c7ba298a654bb78f5b5
Yiling Cao <yiling.cao@gmail.com>
2012-07-07 02:11:00 +0800

    fix remote_you-dl.rb shell script syntax error.

--- remote/remote_you-dl.sh
+++ remote/remote_you-dl.sh
@@ -5,7 +5,7 @@ j=1
 while [ $res -gt 0 ] ; do
   cd ~/$1 && ~/youtube-dl -t http://www.youtube.com/watch?v=$1 && sha1sum * > $1-sha1.txt
   res=$?
-  if [ $res -eq 0] ; then #if 0, exit, scuess
+  if [ $res -eq 0 ] ; then #if 0, exit, scuess
     exit 0
   else
     if [ $j -gt 2 ] ; then #if too many retries, return 10
